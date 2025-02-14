1c5b40675776f7d590777125da4ab1323478d033
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-14 22:19:34 +0700

    Add files via upload

---
+++ Bai2.cpp
@@ -0,0 +1,47 @@
+﻿/*Viết chương trình dùng câu lệnh while để nhập và xuất giá trị cho một mảng số nguyên
+  gồm 4 hàng và 3 cột.
+
+  Gợi ý: dựa trên tính chất câu lệnh while và for để xây dựng lại phần nhập và xuất mảng 2
+  chiều.			*/
+#include<iostream>
+using namespace std;
+
+
+int main() {
+	const int MAX_R = 4, MAX_C = 3;
+
+	int arr[MAX_R][MAX_C];
+
+	//Nhap gia tri cua mang 
+	int i = 0;
+
+	while (i < MAX_R) {
+		int j = 0;
+		while (j < MAX_C) {
+			cout << "Nhap vao mang a[ " << i + 1 << " ] [ "<<j + 1 << " ] : ";
+			cin >> arr[i][j];
+			j++;
+		}
+		i++;
+		
+	}
+
+	//Xuat gia tri cua mang
+	i = 0;
+	while (i < MAX_R) {
+
+		int j = 0;
+
+		while (j < MAX_C) {
+			cout << arr[i][j] << " ";
+			j++;
+		}
+		i++;
+		cout << endl;
+	}
+
+
+	system("pause");
+
+	return 0;
+}
