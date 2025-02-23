92ae53c4b765d0c96a9e6775c6f10c5fcf4b46f1
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-23 17:34:22 +0700

    Add files via upload

---
+++ bai4_DQTL.cpp
@@ -0,0 +1,23 @@
+﻿/*	Viết hàm đệ qui để chuyển 1 số nguyên dương từ hệ thập phân sang
+	nhị phân. Sau đó viết chương trình để kiểm tra hàm đã viết.*/
+#include<iostream>
+using namespace std;
+
+
+
+int decToBin(int n) {
+	if (n == 0)
+		return 0;
+	return n % 2 + 10 * decToBin(n / 2);
+}
+int main() {
+	int n;
+
+	cin >> n;
+
+	cout << decToBin(n) << endl;
+
+	system("pause");
+
+	return 0;
+}
