e844c3481657283ad2eac6c95aae30aa658fe9a9
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-02-23 14:54:14 +0700

    Add files via upload

---
+++ bai2_DQTL.cpp
@@ -0,0 +1,21 @@
+﻿/*	Viết hàm đệ qui để đếm số chữ số của 1 số nguyên dương. Sau đó
+	viết chương trình để kiểm tra hàm vừa viết						*/
+#include<iostream>
+using namespace std;
+
+
+int demSoNguyen(int n) {
+	if (n / 10 == 0)
+		return 1;
+	return 1 + demSoNguyen(n / 10);
+}
+int main() {
+	int n;
+
+	cin >> n;
+
+	cout << demSoNguyen(n) << endl;
+	system("pause");
+
+	return 0;
+}
