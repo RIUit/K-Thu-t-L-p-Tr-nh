24507fb6bf75394f05dada10dd89bb45fb63dbe3
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-22 19:54:55 +0700

    Add files via upload

---
+++ bai1_DQ.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int tinhTong(int n){
+	if(n==1) return 1;
+	return n+tinhTong(n-1);
+}
+int main(){
+	int n;
+	cin>>n;
+	cout<<tinhTong(n)<<endl;
+	return 0;
+}
