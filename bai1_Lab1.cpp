427913b1b3b1dba259f59c1422b21e8d5c9c8cf9
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-26 22:49:34 +0700

    Add files via upload

--- bai1_Lab1.cpp
+++ bai1_Lab1.cpp
@@ -1,38 +1,38 @@
 #include<iostream>
-#include<cmath>
 using namespace std;
 
 
-int tong1(int n){
-	if(n==1) return 1;
-	return n+tong1(n-1);
+int tongN(int n){
+	if(n == 1) return 1;
+	return n + tongN(n - 1);
 }
-int tong2(int n){
-	if(n==1) return 1;
-	return n*n + tong2(n-1);
+int tongNB(int n){
+	if(n == 1) return 1;
+	return n * n + tongNB(n - 1);
 }
-double tong3(int n){			//Luu y : Kieu ham gi return dung kieu do
-	if(n==1) return 1.0;			//Nen va nen n vao mac dinh la le,co the kiem tra chan o int main()
-	return 1.0/n +tong3(n-2);
+double tongTPL(int n){
+	if(n == 1) return 1.0;
+	return 1.0 / n + tongTPL(n - 2);
 }
-int tinhGiaiThua(int n){
-	if(n==0) return 1;
-	return n*tinhGiaiThua(n-1);
+int giaithua(int n){
+	if(n == 0) return 1;
+	return n * giaithua(n - 1);
 }
-int tong4(int n){//Tong cac giai thua 
-	if(n==1) return 1;
-	return tinhGiaiThua(n)+tong4(n-1);
+int tongGiaiThua(int n){
+	if(n == 1) return 1;
+	return giaithua(n) + tongGiaiThua(n-1);
 }
 int main(){
 	int n;
+	cout<<"==========Tinh cac tong bang de quy==========\n";
+	cout<<"Nhap so nguyen duong : ";
 	cin>>n;
-	cout<<"Tong so nguyen toi n : "<<tong1(n)<<endl;
-	cout<<"Tong cac phan tu n*n : "<<tong2(n)<<endl;
+	
+	cout<<"Tong cac so nguyen toi "<<n<<" 			  : "<<tongN(n)<<endl;
+	cout<<"Tong cac so nguyen binh phuong toi "<<n<<" 		  : "<<tongNB(n)<<endl;
 	int tam=n;
-	if(tam%2==0) tam--;
-	cout<<"Tong 1/n le : "<<tong3(tam)<<endl;
-	cout<<"Tong cac giai thua : "<<tong4(n)<<endl;
-	
-	
+	if(n % 2 == 0) tam--;
+	cout<<"Tong cac thap phan le toi "<<n<<"			  : "<<tongTPL(tam)<<endl;
+	cout<<"Tong cac giai thua  toi "<<n<<" 		  	  : "<<tongGiaiThua(n)<<endl;
 	return 0;
 }
