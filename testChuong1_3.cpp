678764fe54f97011aa36a4e88776ede896455040
Trịnh Trịnh <trinhkimngan2k6@gmail.com>
2025-03-29 22:06:44 +0700

    Add files via upload

--- testChuong1_3.cpp
+++ testChuong1_3.cpp
@@ -3,80 +3,82 @@
 using namespace std;
 
 
-void nhapMang(int*a,int n){
-	if(n == 0) return;
+	
+void nhap(int*a,int n){
+	if(n==0) return;
+	cout<<"Nhap phan tu ";
 	cin>>*a;
-	nhapMang(a+1,n-1);
+	nhap(a+1,n-1);
 }
-void xuatMang(int*a,int n){
-	if(n == 0) return;
+void xuat(int*a,int n){
+	if(n==0) return;
 	cout<<*a<<" ";
-	xuatMang(a+1,n-1);
+	xuat(a+1,n-1);
 }
-int*maxPT(int*a,int n){
-	if(n == 1) return a;
-	return (*a > *maxPT(a+1,n-1) )? a : maxPT(a+1,n-1); 
+int*maxMang(int*p,int n){
+	if(n==1) return p;
+	return (*p > *maxMang(p+1,n-1))? p : maxMang(p+1,n-1);
 }
-int*minPT(int*a,int n){
-	if(n == 1) return a;
-	return (*a < *minPT(a+1,n-1) )? a : minPT(a+1,n-1); 
+int*minMang(int*p,int n){
+	if(n==1) return p;
+	return (*p < *minMang(p+1,n-1))? p : minMang(p+1,n-1);
 }
 void daoNguoc(int*p,int l,int r){
 	if(l>=r) return;
 	swap(p[l],p[r]);
-	daoNguoc(p,l+1,r-1);
+	return  daoNguoc(p,l+1,r-1);
 }
 void toHex(int n,string &hex){
-	char bang[]="0123456789ABCDEF";
+	char bang[] ="0123456789ABCDEF";
 	if(n<16){
-		hex+=bang[n];
+		hex+=bang[n%16];
 		return;
 	}
 	toHex(n/16,hex);
-	hex+=bang[n%16];
+	hex+=bang[n%16]; 
 }
-int hexToDec(string hex,int doDai =0){
-	if(doDai == hex.length()) return 0;
-	char kt = hex[doDai];
-	int thapphan;
+int hexToDec(string hex,int dodai=0){  // 3E3
+	if(dodai == hex.length()) return 0;
+	char kt=hex[dodai];
+	int tp;
 	if(kt>='0'&&kt<='9')
-		thapphan=kt-'0';
+		tp=kt-'0';
 	else if(kt>='A'&&kt<='F')
-		thapphan=kt-'A'+10;
-	return thapphan*pow(16,doDai)+hexToDec(hex,doDai+1);	
+		tp=kt-'A'+10;
+	return tp*pow(16,hex.length()-1-dodai) + hexToDec(hex,dodai+1);
 }
 int main(){
 	int n;
-	
-	cout<<"Nhap so nguyen duong : ";
+	cout<<"Nhap so nguyen duong n : ";
 	cin>>n;
-	int*a=new int [n];
 	
-	nhapMang(a,n);
+	int*a=new int[n];
 	
-	cout<<"Mang sau khi nhap \n";
-	xuatMang(a,n);
+	nhap(a,n);
+	xuat(a,n);
 	cout<<endl;
 	
-	cout<<"Pt lon nhat cua mang: "<<*maxPT(a,n)<<endl;
-	cout<<"Pt nho nhat cua mang: "<<*minPT(a,n)<<endl;
-	cout<<"Dao nguoc mang: "<<endl;
+	cout<<"Dao nguoc mang \n";
 	daoNguoc(a,0,n-1);
-	xuatMang(a,n);
+	xuat(a,n);
 	cout<<endl;
-	 
-	string s=""; //Khai bao rong
-	int x;
-	cout<<"Nhap so thap phan can chuyen sang he 16 : ";
-	cin>>x;
-	if(x == 0)
+	
+	cout<<"Max pt mang : "<<*maxMang(a,n)<<endl;
+	cout<<"Min pt mang : "<<*minMang(a,n)<<endl;
+	cout<<"Vi tri Min pt mang : "<<minMang(a,n)<<endl;
+	
+	int sothapphan;
+	string s="";
+	cout<<"Nhap so thap phan : ";
+	cin>>sothapphan;
+	
+	if(sothapphan == 0)
 		cout<<"0";
 	else{
-		
-		toHex(x,s);
-		cout<<"So thap phan da chuyen sang he 16 : "<<s<<endl;	
+		toHex(sothapphan,s);
+		cout<<"So Hex cua so "<<sothapphan<<" : "<<s<<endl;
 	}
-	
-	cout<<"So thap luc phan "<<s<<" doi ve thap phan : "<<hexToDec(s)<<endl;
+	cout<<"Doi "<<s<<" sang hex : "<<hexToDec(s)<<endl;
+	delete []a;
 	return 0;
 }
